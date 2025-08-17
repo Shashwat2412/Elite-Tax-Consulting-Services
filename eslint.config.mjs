83f6e44e34b8faec2f5a1c0a2b6b9270a139ecca
Shashwat Mishra <shashwatmishra2403@gmail.com>
2025-08-17 16:38:12 +0530

    commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,32 +33,4 @@
 
 // export default eslintConfig;
 
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  {
-    files: ["**/*.js", "**/*.ts", "**/*.jsx", "**/*.tsx"],
-    rules: {
-      // Disabled rules
-      "no-unused-vars": "off",
-      "no-redeclare": "off",
-      "no-param-reassign": "off",
-      "no-console": "off",
-      "no-undef": "off",
-      // Add more rules here if needed
-    },
-  },
-];
-
-export default eslintConfig;
+export default [];
